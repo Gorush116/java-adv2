063b4d445a9cae4e37617fcd58e1472470f3a317
Gorush116 <gorush34@gmail.com>
2025-04-16 08:53:13 +0900

    외부 자원을 실행할 때 예외 처리를 제대로 하지 않으면 근본적인 원인과 자원 누수가 발생할 가능성이 생긴다.

---
+++ src/network/tcp/autoclosable/CallException.java
@@ -0,0 +1,9 @@
+package network.tcp.autoclosable;
+
+public class CallException extends Exception {
+
+    public CallException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/network/tcp/autoclosable/CloseException.java
@@ -0,0 +1,9 @@
+package network.tcp.autoclosable;
+
+public class CloseException extends Exception {
+
+    public CloseException(String message) {
+        super(message);
+    }
+
+}
