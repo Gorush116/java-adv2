f30fd5caa7199485843963ed5d52c4ef600977ce
Gorush116 <gorush34@gmail.com>
2025-04-18 08:52:40 +0900

    try-with-resources에서 외부에서 받아오는 인스턴스도 적용이 가능하다.

--- src/network/tcp/v5/SessionV5.java
+++ src/network/tcp/v5/SessionV5.java
@@ -1,4 +1,4 @@
-package network.tcp.v4;
+package network.tcp.v5;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -8,25 +8,20 @@ import java.net.Socket;
 import static network.tcp.SocketCloseUtil.closeAll;
 import static util.MyLogger.log;
 
-public class SessionV4 implements Runnable {
+public class SessionV5 implements Runnable {
 
     private final Socket socket;
 
-    public SessionV4(Socket socket) {
+    public SessionV5(Socket socket) {
         this.socket = socket;
     }
 
 
     @Override
     public void run() {
-
-        DataInputStream input = null;
-        DataOutputStream output = null;
-
-        try {
-
-            input = new DataInputStream(socket.getInputStream());
-            output = new DataOutputStream(socket.getOutputStream());
+        try(socket;
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
 
             while (true) {
                 // 클라이언트로부터 문자 받기
@@ -43,12 +38,10 @@ public class SessionV4 implements Runnable {
                 output.writeUTF(toSend);
                 log("client <- server : " + toSend);
             }
-
         } catch (IOException e) {
             log(e);
-        } finally {
-            closeAll(socket, input, output);
-            log("연결 종료 : " + socket);
         }
+
+        log("연결 종료 : " + socket.isClosed());
     }
 }
