e506bb90baffab7870763731ca08344fa6018380
Gorush116 <gorush34@gmail.com>
2025-05-14 08:55:21 +0900

    HTTP Server 구축 - 1

---
+++ src/was/v1/HttpServerV1.java
@@ -0,0 +1,14 @@
+package was.v1;
+
+public class HttpServerV1 {
+
+    private final int port;
+
+    public HttpServerV1(int port) {
+        this.port = port;
+    }
+
+    public void start() {
+
+    }
+}
